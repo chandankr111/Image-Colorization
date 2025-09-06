f7ccc8120bf39c7de58db576de7c8526f6c39fd2
chandankr111 <122672735+chandankr111@users.noreply.github.com>
2025-09-06 18:16:49 +0530

    .

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from torchvision import transforms
 from model import MainModel 
 import numpy as np
 from skimage.color import rgb2lab, lab2rgb
+import warnings
+warnings.filterwarnings("ignore", message="Tried to instantiate class '__path__._path'")
 
 
 # Cache the model to avoid reloading it every time
@@ -38,15 +40,22 @@ if uploaded_image:
     # --- Colorization Inference ---
     if st.button("🎨 Colorize"):
         with torch.no_grad():
-            # Predict ab channels using the model
             fake_ab = model.net_G(input_tensor)
-            L = input_tensor[0][0].cpu().numpy() * 0.5 + 0.5  
-            ab = fake_ab[0].cpu().numpy() * 0.5 + 0.5        
 
+            # Convert L back to [0, 100]
+            L = input_tensor[0][0].cpu().numpy() * 50 + 50  
+
+            # Convert ab back to [-128, 127]
+            ab = fake_ab[0].cpu().numpy().transpose(1, 2, 0) * 128  
+
+            # Combine LAB channels
             lab = np.zeros((256, 256, 3))
-            lab[:, :, 0] = L * 100                            
-            lab[:, :, 1:] = (ab.transpose(1, 2, 0) - 0.5) * 256  
+            lab[:, :, 0] = L
+            lab[:, :, 1:] = ab
+
+            # Convert to RGB
             rgb = lab2rgb(lab)
             rgb_image = (rgb * 255).astype(np.uint8)
+
 
             st.image(rgb_image, caption="🌈 Colorized Output", use_column_width=False, width=256)
